603130996d74de5383ddd64e8c64a2a579ac3aac
datle30 <146958218+datle30@users.noreply.github.com>
2025-06-09 11:16:06 +0700

    Add files via upload

---
+++ Interface/Point.java
@@ -0,0 +1,39 @@
+public class Point {
+    private double pointX;
+    private double pointY;
+
+    /**
+     * Point().
+     */
+    public Point(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    /**
+     * Getter and Setter.
+     */
+    public double getPointX() {
+        return pointX;
+    }
+
+    public void setPointX(double pointX) {
+        this.pointX = pointX;
+    }
+
+    public double getPointY() {
+        return pointY;
+    }
+
+    public void setPointY(double pointY) {
+        this.pointY = pointY;
+    }
+
+    /**
+     * distance.
+     */
+    public double distance(Point other) {
+        return Math.sqrt((this.pointX - other.pointX) * (this.pointX - other.pointX)
+                + (this.pointY - other.pointY) * (this.pointY - other.pointY));
+    }
+}
