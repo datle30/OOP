1dce18e51047060c3c9a48b47bc860bfd2aec25e
datle30 <146958218+datle30@users.noreply.github.com>
2025-06-09 11:15:20 +0700

    Add files via upload

---
+++ GenericProgram/Person.java
@@ -0,0 +1,76 @@
+import java.util.Objects;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private String address;
+
+    /**
+     * Person.
+     */
+    public Person() {
+    }
+
+    /**
+     * Person().
+     */
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    /**
+     * getter and setter.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * compareTo.
+     */
+    @Override
+    public int compareTo(Person person) {
+        if (this.equals(person)) {
+            return Integer.compare(this.age, person.getAge());
+        } else {
+            return name.compareTo(person.getName());
+        }
+    }
+
+    /**
+     * equals.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+}
