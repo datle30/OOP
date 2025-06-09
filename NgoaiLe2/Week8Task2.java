fcb703b57758fc5c4350cfe607c463bb020fa3ed
datle30 <146958218+datle30@users.noreply.github.com>
2025-06-09 11:18:36 +0700

    Add files via upload

---
+++ NgoaiLe2/Week8Task2.java
@@ -0,0 +1,87 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Week8Task2 {
+    public void nullPointerEx() throws NullPointerException {
+        String s = null;
+        s.length();
+    }
+
+    public void arrayIndexOutOfBoundsEx() throws ArrayIndexOutOfBoundsException {
+        String[] arr = new String[12];
+        arr[19] = "";
+    }
+
+    public void arithmeticEx() {
+        int a = 7 / 0;
+    }
+
+    public void fileNotFoundEx() throws FileNotFoundException {
+        throw new FileNotFoundException();
+    }
+
+    public void ioEx() throws IOException {
+        throw new IOException();
+    }
+
+    /**
+     * nullPointerExTest.
+     */
+    public String nullPointerExTest() {
+        try {
+            nullPointerEx();
+            return "Không có lỗi";
+        } catch (NullPointerException e) {
+            return "Lỗi Null Pointer";
+        }
+    }
+
+    /**
+     * arrayIndexOutOfBoundsExTest.
+     */
+    public String arrayIndexOutOfBoundsExTest() {
+        try {
+            arrayIndexOutOfBoundsEx();
+            return "Không có lỗi";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Lỗi Array Index Out of Bounds";
+        }
+    }
+
+    /**
+     * arithmeticExTest.
+     */
+    public String arithmeticExTest() {
+        try {
+            arithmeticEx();
+            return "Không có lỗi";
+        } catch (Exception e) {
+            return "Lỗi Arithmetic";
+        }
+    }
+
+    /**
+     * fileNotFoundExTest.
+     */
+    public String fileNotFoundExTest() {
+        try {
+            fileNotFoundEx();
+            return "Không có lỗi";
+        } catch (FileNotFoundException e) {
+            return "Lỗi File Not Found";
+        }
+    }
+
+    /**
+     * ioExTest.
+      */
+    public String ioExTest() {
+        try {
+            ioEx();
+            return "Không có lỗi";
+        } catch (Exception e) {
+            return "Lỗi IO";
+        }
+    }
+}
